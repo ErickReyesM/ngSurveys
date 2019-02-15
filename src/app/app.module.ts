df155556449bb0bf0d4461f8dd9b6f73a07e88fe
ereyesm <ereyes@intersysconsulting.com>
2019-02-15 10:49:30 -0600

    adding firebase to the project!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,18 @@ import { ThirdPageComponent } from './third-page/third-page.component';
 import { SurveyQuestionsComponent } from './survey-questions/survey-questions.component';
 import { DataService } from 'src/seervices/data.service';
 
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyCMsiQRCVEa-qzxA-bnt-Jk5FXyOQgFrBw",
+  authDomain: "sondaggio-ea6bf.firebaseapp.com",
+  databaseURL: "https://sondaggio-ea6bf.firebaseio.com",
+  projectId: "sondaggio-ea6bf",
+  storageBucket: "",
+  messagingSenderId: "22946728796"
+};
+firebase.initializeApp(config);
+
 const appRoutes: Routes = [
   { path: 'first-page', component: FirstPageComponent },
   { path: 'second-page', component: SecondPageComponent },
