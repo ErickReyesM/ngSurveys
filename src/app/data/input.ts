7a57532032b827370cba6ee0336023da6d1d11df
Erick Reyes <erick.reyesm04@gmail.com>
2019-03-03 18:59:34 -0600

    craeating functionality of charting data

---
+++ src/app/data/input.ts
@@ -0,0 +1,62 @@
+export default[
+    {
+        created: '2019-03-02T21:58:22.686Z',
+        surveyId: 'jK5DeQu5JP5vQFSnbmne',
+        input: [
+            {
+                type: 'Elección',
+                value: 'eleccion 2'
+            }
+        ]
+    },
+    {
+        created: '2019-03-02T22:00:22.686Z',
+        surveyId: 'jK5DeQu5JP5vQFSnbmne',
+        input: [
+            {
+                type: 'Elección',
+                value: 'eleccion 1'
+            }
+        ]
+    },
+    {
+        created: '2019-03-02T16:58:22.686Z',
+        surveyId: 'jK5DeQu5JP5vQFSnbmne',
+        input: [
+            {
+                type: 'Elección',
+                value: 'eleccion 3'
+            }
+        ]
+    },
+    {
+        created: '2019-03-02T17:30:22.686Z',
+        surveyId: 'jK5DeQu5JP5vQFSnbmne',
+        input: [
+            {
+                type: 'Elección',
+                value: 'eleccion 4'
+            }
+        ]
+    },
+    {
+        created: '2019-03-02T21:58:22.686Z',
+        surveyId: 'jK5DeQu5JP5vQFSnbmne',
+        input: [
+            {
+                type: 'Elección',
+                value: 'eleccion 1'
+            }
+        ]
+    },
+    {
+        created: '2019-03-02T18:10:22.686Z',
+        surveyId: 'jK5DeQu5JP5vQFSnbmne',
+        input: [
+            {
+                type: 'Elección',
+                value: 'eleccion 2'
+            }
+        ]
+    }
+]
--- src/app/third-page/third-page.component.ts
+++ src/app/third-page/third-page.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as jsPDF from 'jspdf';
+import input from '../data/input';
+
+export interface InputType{
+  type: string,
+  value?: string
+  options?: any[]
+}
 
 @Component({
   selector: 'app-third-page',
@@ -7,12 +14,13 @@ import * as jsPDF from 'jspdf';
   styleUrls: ['./third-page.component.css']
 })
 
-
 export class ThirdPageComponent implements OnInit{
+
+  inputCollection:{created:string, surveyId:string, input:InputType[]}[];
 
   @ViewChild('content') content: ElementRef;
 
-  /*Bar Chart Data */
+  /*Bar Chart Data 
   public barChartDatasets = [
     {data: [654,321,98,654,21,87,654,322], label: 'Series A'},
     {data: [123,54,787,121,127,96,85,741], label: 'Series B'}
@@ -25,27 +33,79 @@ export class ThirdPageComponent implements OnInit{
   public barChartLegend = true;
   public barChartType = 'bar';
 
-  /*Doughnut Chart Data */
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
-  public doughnutChartType:string = 'doughnut';
+  /*Doughnut Chart Data*/
+public doughnutChartLabels:string[] = [];
+public doughnutChartData:number[] = [];
+public doughnutChartType:string = 'doughnut';
 
-  /**Line Chart Data */
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    responsive: true
-  };
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
+public createdDate:any[] = [];
+public lineChartData:Array<any> = [];
+public lineChartLabels:Array<any> = [];
+public lineChartOptions:any = {responsive: true};
+public lineChartLegend:boolean = true;
+public lineChartType:string = 'line';
+public valueInputQ:Array<any> = [];
+public valueInputLabels:Array<any> = [];
+private unique:Array<any> = [];
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.inputCollection = input;
+    //console.log(this.inputCollection);
+    this.inputCollection.forEach(data =>{
+      let sToD = data.created;
+      let dateCreated = new Date(sToD);
+      this.createdDate.push(dateCreated.getHours());
+      data.input.forEach(input =>{
+        this.valueInputQ.push(input.value);
+      })
+    });
+    this.lineChartData = [
+      {data: this.countByWeek(this.createdDate), label: 'Encuestas'}
+    ];
+     this.lineChartLabels = 
+     ['8-9','9-10','10-11','11-12','12-13','13-14','14-15','15-16','16-17','17-18','18-19','19-20'];
+     let x = (names) => names.filter((v,i) => names.indexOf(v) === i);
+     console.log(this.valueInputQ);
+     this.doughnutChartData = this.countInputValue(this.valueInputQ);
+     this.doughnutChartLabels = x(this.valueInputQ);
+  }
+
+  countInputValue(array_elements:any[]):any[] {
+    let count:any[] = [];
+    var current = null;
+    var cnt = 1;
+    array_elements.sort();
+    for (var i = 0; i < array_elements.length; i++) {
+      current = array_elements[i+1];
+        if (array_elements[i] != current) {
+          count.push(cnt);
+          cnt = 1;
+        } else {
+            cnt++;
+        }
+      }
+    return count;
+}
+
+  countByWeek(array_elements:any[]):any[] {
+    let hours:Array<number> = array_elements.concat([8,9,10,11,12,13,14,15,16,17,18,19,20]);
+    let count:any[] = [];
+    var current = null;
+    var cnt = 0;
+    hours.sort((a, b) => a - b);
+    for (var i = 0; i < hours.length; i++) {
+      current = hours[i+1];
+        if (hours[i] != current) {
+          count.push(cnt);
+          cnt = 0;
+        } else {
+            cnt++;
+        }
+      }
+    return count;
+}
 
   public onDownloadPDF(){
     let doc = new jsPDF();
